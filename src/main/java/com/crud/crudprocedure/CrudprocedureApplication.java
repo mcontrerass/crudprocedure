81101ac14b953574902c8cc921c5cde04242f5ee
Mauricio Contreras <mcontrerass@gmail.com>
2022-01-03 19:07:15 -0300

    Plantilla inicial

---
+++ src/main/java/com/crud/crudprocedure/CrudprocedureApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crudprocedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudprocedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudprocedureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/crudprocedure/CrudprocedureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudprocedure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudprocedureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
